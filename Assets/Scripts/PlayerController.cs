590389af4c38f5a1fae705c0adf1ee43b7c0e363
iOSDevLog <iosdevlog@iosdevlog.com>
2019-12-12 13:38:03 +0800

    feat(player): move

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 15;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        if (moveSpeed <= 0)
+        {
+            moveSpeed = 15;
+        }
+    }
+
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float EPSILON = 0.1f;
+
+        if (Math.Abs(horizontalMove) > EPSILON)
+        {
+            rb.velocity = new Vector2(horizontalMove * moveSpeed, rb.velocity.y);
+        }
+    }
+}
